c05884e47a4268ac0fee0d413b04d3ca34cfb072
Paweł Jański <pjans@wp.pl>
2017-07-17 11:50:43 +0200

    reducers added

---
+++ client/reducers/index.js
@@ -0,0 +1,13 @@
+import { combine } from 'redux';
+import { routerReducer } from 'react-router-redux';
+
+import posts from './posts';
+import comments from './comments';
+
+const rootReducer = combineReducers({
+    posts,
+    comments,
+    router: routerReducer
+});
+
+export default rootReducer;
