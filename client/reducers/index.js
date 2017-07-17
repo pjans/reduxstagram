c8c2b156d1c73c37ff9f26df3329b4abe72c3b89
Paweł Jański <pjans@wp.pl>
2017-07-17 15:49:28 +0200

    store integrated with router

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,13 +1,9 @@
-import { combine } from 'redux';
+import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import posts from './posts';
 import comments from './comments';
 
-const rootReducer = combineReducers({
-    posts,
-    comments,
-    router: routerReducer
-});
+const rootReducer = combineReducers({posts, comments, routing: routerReducer });
 
 export default rootReducer;
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,6 +1,6 @@
-function posts(state = [], action){
-    console.log(state, action);
-    return state;
+function posts(state = [], action) {
+  console.log(state, action);
+  return state;
 }
 
 export default posts;
