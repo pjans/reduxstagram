3dcee14ad1826fdbb85df84f0e21d0390fddcfa3
Paweł Jański <pjans@wp.pl>
2017-07-18 14:12:58 +0200

    first reducer

--- client/components/Photo.js
+++ client/components/Photo.js
@@ -26,7 +26,7 @@ const Photo = React.createClass({
                 <figcaption>
                     <p>{post.caption}</p>
                     <div className="control-buttons">
-                        <button className="likes">
+                        <button onClick={this.props.increment.bind(null, i)} className="likes">
                             &hearts; {post.likes}
                         </button>
                         <Link className="button">
